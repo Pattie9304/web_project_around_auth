bcd15996e97d0737a101c074f0076a42759b6415
PatriciaHiguera <patyhiguera@hotmail.com>
2025-10-15 19:03:08 -0600

    Nueva versión de la red social que incluye las registro, inicio de sesión y cierre de sesión

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # Tripleten web_project_around_auth
+## Nombre del proyecto: Sitio Web Around the US - Tripleten
+### Descripción del proyecto: 
+
+Creación de funcionalidad de registro, verificación y cierre de sesión con Express.js
+
+  * Se crean las páginas de registro e inicio de sesión
+  * Se configuran las hojas de estilo de dichas páginas
+  * Se agregan las funcionalidades de autenticación y verificación
+
+***
+
+### Tecnologías usadas:
+  El sitio web esta diseñado con las siguientes tecnologías:
+
+  * El lenguaje de programación para escribir la lógica y funcionalidad del frontend es **express.js** y la del backend es **Node Js**.
+
+***
+
+### Despliegue en la web:
+  La página web esta desplegada en Github Pages y se aloja en el siguiente link: **AGREGAR link**
+***
+
+
+## Rutas:
+
+* **GET /users**: Obtiene todos los usuarios.
+
+* **GET /users/:id**: Obtiene un usuario específico por el Id.
+
+* **POST /users**: Crea un usuario.
+
+* **PATCH /users/me**: Actualiza información del usuario.
+
+* **PATCH /users/me/avatar**: Actualiza el avatar del usuario.
+
+* **GET /cards**: Obtiene todas las tarjetas.
+
+* **POST /cards**: Crea una tarjeta.
+
+* **DELETE /cards/:cardId**: Elimina una tarjeta específica.
+
+* **PUT /cards/:cardId/likes**: Agrega un like a una tarjeta.
+
+* **DELETE /cards/:cardId/likes**: Elimina un like de una tarjeta.
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,82 @@
+import { useContext } from 'react';
+import Popup from '../Main/Popup/Popup.jsx';
+import NewCard from '../Main/Popup/NewCard/NewCard.jsx';
+import EditProfile from '../Main/Popup/EditProfile/EditProfile.jsx';
+import EditAvatar from '../Main/Popup/EditAvatar/EditAvatar.jsx';
+import RemoveCard from '../Main/Popup/RemoveCard/RemoveCard.jsx';
+import Card from '../Main/Cards/Card.jsx';
+import ImagePopup from '../Main/Popup/ImagePopup/ImagePopup.jsx';
+import CurrentUserContext from '@contexts/CurrentUserContext.js';
+import { validationConfig } from '@utils/validationConfig.js';
+
+function Main(props) {
+    const { popup, onOpenPopup, onClosePopup, cards, onCardLike, onCardDelete } = props;
+    const newCardPopup = { title: "Nuevo lugar", children: <NewCard validationConfig={validationConfig} /> };
+    const editProfolePopup = { title: "Nuevo lugar", children: <EditProfile validationConfig={validationConfig} /> };
+    const editAvatarPopup = { title: "Nuevo avatar", children: <EditAvatar validationConfig={validationConfig} /> };
+    const { currentUser } = useContext(CurrentUserContext);
+
+    function handleCardClick(card) {
+        onOpenPopup({ children: <ImagePopup card={card} /> });
+    }
+
+    function handleTrashClick(card) {
+        onOpenPopup({ title: "¿Estás seguro?", children: <RemoveCard onCardDelete={onCardDelete} card={card}/> });
+    }
+
+    return (
+        <main className="main">
+            <div className="main__profile">
+                <div className="main__content-image">
+                    <img src={currentUser.avatar}
+                    alt="profile-image"
+                    className="main__profile-image"
+                    />
+                    <button 
+                    type="button"
+                    className="main__button main__button_img"
+                    onClick={() => onOpenPopup(editAvatarPopup)}
+                    >
+                        &#x1F58C;
+                    </button>
+                </div>
+                <div className="main__content-paragraph">
+                    <p className="main__paragraph main__paragraph_name">{currentUser.name}</p>
+                    <p className="main__paragraph main__paragraph_job">{currentUser.about}</p>
+                    <button
+                    type="button"
+                    className="main__button main__button_edit"
+                    onClick={() => onOpenPopup(editProfolePopup)}
+                    >
+                    &#x1F58C;
+                    </button>
+                </div>
+                <button
+                aria-label="Add card" 
+                type="button" 
+                className="main__button main__button_add" 
+                onClick={() => onOpenPopup(newCardPopup)}>
+                &#x1F7A3;
+                </button>
+            </div>
+            <ul className="main__gallery">
+                {cards.map((card) => (
+                    <Card
+                    key={card._id}
+                    card={card}
+                    handleCardClick={handleCardClick}
+                    onCardLike={onCardLike}
+                    handleTrashClick={handleTrashClick}
+                    />
+                ))}
+            </ul>
+            {popup && (
+                <Popup onClose={onClosePopup} title={popup.title}>
+                {popup.children}
+            </Popup>
+        )}
+        </main>
+    );
+}
+
+export default Main;
